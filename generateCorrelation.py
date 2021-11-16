a2835bc4360f2814dd3d2790152fc368c8af85d0
Owen Evans <db19891@bristol.ac.uk>
2021-11-16 12:51:47 +0000

    median averages and func to list all filenames

--- generateCorrelation.py
+++ generateCorrelation.py
@@ -1,15 +1,13 @@
 import pandas as pd
-
-import LoadLocationData
 import anprCameraRoutes
 import getTrafficData
 import matplotlib.pyplot as plt
-from calcPunctuality import findLateness, findMeanPunct, getMoreBars
+from calcPunctuality import findMeanPunct
 from findLeaveTimes import leaveTimes
 from importTimetable import onlyNeededTimetable
 from datetime import datetime
 from getTrafficData import getTrafficForRange
-
+from LoadLocationData import listFilenames
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 
@@ -17,7 +15,7 @@ import matplotlib.pyplot as plt
 
 #Defining Constants
 
-FILENAMES = ['LocationDataLog27-10-2021,19;26;47RunTime32400.json','LocationDataLog19-10-2021,18;16;05RunTime28800.json','LocationDataLog26-10-2021,12;20;15RunTime14400.json','LocationDataLog26-10-2021,19;38;25RunTime25200.json']
+FILENAMES = listFilenames()
 ANPRFILENAME = 'dim-journey-links.json'
 
 #Defining variables
@@ -33,7 +31,7 @@ speeds = getTrafficForRange(timeRange, roadName, date)
 
 times = leaveTimes(FILENAMES, busStopName, lines, direction, stopLocation)
 timetable = onlyNeededTimetable(direction, lines, busStopName)
-hours, means = findMeanPunct(timetable, times, lines)
+hours, means, medians, vars = findMeanPunct(timetable, times, lines)
 rCoeff = pearsonr(speeds, means)
 print(rCoeff)
 plt.scatter(speeds, means)
--- plotPunctuality.py
+++ plotPunctuality.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from LoadLocationData import listFilenames
 from calcPunctuality import findLateness, findMeanPunct, getMoreBars
 from findLeaveTimes import leaveTimes
 from importTimetable import onlyNeededTimetable
@@ -13,17 +14,19 @@ def plotLatenessHist(timetable, hours):
     plt.show()
 
 def plotMeanLateness(timetable, hours, line, stop):
-    times, means = findMeanPunct(timetable, hours, line)
+    times, means, medians, vars = findMeanPunct(timetable, hours, line)
     plt.bar(times, means, width=1.0, align='edge')
+    plt.scatter(times, medians, c='b')
+    #plt.scatter(times+0.5, var)
     plt.axhline(y=0, color='r', linestyle='-')
-    plt.legend(['On time','Punctuality'])
+    plt.legend(['On time', 'A median', 'Punctuality'])
     plt.title('Histogram for the mean minutes all buses leave ' + stop + ' behind schedule for the the hours in the day')
     plt.xlabel('Time of the day')
     plt.ylabel('Time behind schedule/minutes')
     plt.show()
 
 def plotMoreMeanBars(timetable, hours, line, stop):
-    times, means = getMoreBars(timetable, hours, line)
+    times, means, medians, vars = getMoreBars(timetable, hours, line)
     times = [x/4 for x in times]
     plt.bar(times, means, width=0.25, align='edge')
     plt.axhline(y=0, color='r', linestyle='-')
@@ -35,7 +38,7 @@ def plotMoreMeanBars(timetable, hours, line, stop):
 
 if __name__ == '__main__':
     ''' Parameters to change '''
-    filenames = ['LocationDataLog27-10-2021,19;26;47RunTime32400.json','LocationDataLog19-10-2021,18;16;05RunTime28800.json','LocationDataLog26-10-2021,12;20;15RunTime14400.json','LocationDataLog26-10-2021,19;38;25RunTime25200.json']
+    filenames = listFilenames()
     busStopName = 'Bristol College Green (P1)'
     # list all the bus lines being considered
     lines = ['4','3']
@@ -47,5 +50,5 @@ if __name__ == '__main__':
     times = leaveTimes(filenames, busStopName, lines, direction, stopLocation)
     timetable = onlyNeededTimetable(direction, lines, busStopName)
     #plotLatenessHist(timetable, times)
-    #plotMeanLateness(timetable, times, lines, busStopName)
-    plotMoreMeanBars(timetable, times, lines, busStopName)
+    plotMeanLateness(timetable, times, lines, busStopName)
+    #plotMoreMeanBars(timetable, times, lines, busStopName)
